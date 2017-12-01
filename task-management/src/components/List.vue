7bf8059d9c0f9638c047b408b1126a4a21367049
andreadelreux <andreadelre@Admins-MacBook-Pro.local>
2017-12-01 11:00:57 +0100

    update
    update

---
+++ task-management/src/components/List.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="assets--list">
+    <select class="" name="" v-model="selected">
+      <option value="origin" disabled>{{placeholder}}</option>
+      <option v-for="option in options" :value="option">
+        {{option}}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    selected: {
+      default: 'origin'
+    },
+    options: {
+      default: function () {
+        return ['option 1', 'option 2', 'option 3']
+      }
+    },
+    placeholder: {
+      default: 'Please write placeholder-props'
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
